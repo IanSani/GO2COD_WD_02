9a039ad91a840a8215bce5bc9bb288bfe4b9f91e
iansani <iansani259@gmail.com>
2024-11-05 15:56:02 +0300

    Added button dsabling

--- script.js
+++ script.js
@@ -101,6 +101,13 @@ function selectAnswer(e){
     }else{
         selectedBtn.classList.add('incorrect');
     }
+    Array.from(answerElement.children).forEach(button=>{
+        if(button.dataset.correct === "true"){
+            button.classList.add('correct');
+        }
+        button.disabled =true;
+    })
+    nextButton.style.display="block";
 }
 
 startQuiz();
