f715723b82d5a58a10dc4ba48b459b99e820131f
iansani <iansani259@gmail.com>
2024-11-05 16:08:59 +0300

    Added score functionality

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score ++;
     }else{
         selectedBtn.classList.add('incorrect');
     }
@@ -109,5 +110,27 @@ function selectAnswer(e){
     })
     nextButton.style.display="block";
 }
+
+function showScore(){
+    questionElement.innerHTML=`You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML= "Play Again";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener('click',()=>{
+    if(currentQuestionIndex< questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+})
 
 startQuiz();
